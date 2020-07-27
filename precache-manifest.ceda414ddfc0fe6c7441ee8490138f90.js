0118f64af8057bbbcb7024c1d16e3f60401a224c
idham099 <ainul.idham_sttpln@yahoo.com>
2020-07-27 16:00:15 +0800

    Updates

---
+++ precache-manifest.ceda414ddfc0fe6c7441ee8490138f90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f5db7bf6cfc9e493a4efaf900baf92e",
+    "url": "/kuliner/index.html"
+  },
+  {
+    "revision": "d8eaeaa1a6a4093a09e4",
+    "url": "/kuliner/static/css/main.874bc5b9.chunk.css"
+  },
+  {
+    "revision": "100da2e20566e5962758",
+    "url": "/kuliner/static/js/2.4c1975d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kuliner/static/js/2.4c1975d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8eaeaa1a6a4093a09e4",
+    "url": "/kuliner/static/js/main.1c88cb0e.chunk.js"
+  },
+  {
+    "revision": "5ba2db4320f7c9a0408a",
+    "url": "/kuliner/static/js/runtime-main.3654a11a.js"
+  },
+  {
+    "revision": "5a7cfcfc09967957fa361d95ee65b756",
+    "url": "/kuliner/static/media/food.5a7cfcfc.jpg"
+  },
+  {
+    "revision": "7230973e6e798cc3ad2e0ab2a5e8190a",
+    "url": "/kuliner/static/media/logoku.7230973e.png"
+  }
+]);
